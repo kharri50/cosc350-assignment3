dc0f96306c693c1c8e1258b74d50adefad84b85d
kharri50 <kharri50@students.towson.edu>
2018-05-11 16:39:30 -0400

    added client configuration steps

--- Assignment 3/src/com/networking/TCPKRClient.java
+++ Assignment 3/src/com/networking/TCPKRClient.java
@@ -1,13 +1,29 @@
 package com.networking;
 import java.io.*;
 import java.net.*;
+import java.util.Scanner;
 
 class TCPKRClient {
 
+    /* Following data members contain the client attributes including:
+            client node number
+            num dvr entries
+            dvr entries (line by line)
+   */
+
+    static int nodeNum;
+    static int numDvrEntries;
+    static int [][] dvrEntries;
+
     public static void main(String argv[]) throws Exception
     {
+
+        readStartUpFile();
+        printClientConfig();
+
         String sentence;
         String modifiedSentence;
+
 
         BufferedReader inFromUser =
                 new BufferedReader(new InputStreamReader(System.in));
@@ -29,6 +45,64 @@ class TCPKRClient {
         System.out.println("FROM SERVER: " + modifiedSentence);
 
         clientSocket.close();
+
+    }
+
+    public static void readStartUpFile() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter a filename for client startup");
+        String fileName = sc.nextLine();
+        System.out.println("Startup file : " + fileName);
+        // code reads the startup file line by line
+
+        int index = 0;
+        String line = "";
+        int dvrIndex = 0;
+        try {
+            FileReader reader = new FileReader(fileName);
+            BufferedReader bufferedReader = new BufferedReader(reader);
+            // read the file line by line
+            while ((line = bufferedReader.readLine()) != null) {
+                if(index==1){
+                    nodeNum = Integer.parseInt(line);
+                }else if (index==2){
+                    numDvrEntries = Integer.parseInt(line);
+                    dvrEntries = new int[numDvrEntries][2];
+                }else if(index > 2 && index <= (2+numDvrEntries)){
+                    // split the string
+                    dvrEntries[dvrIndex] = new int[2];
+                    String [] strDrv = line.split(" ");
+                    for(int i = 0; i<strDrv.length; i++){
+                        // init dvr entry
+                        dvrEntries[dvrIndex][i]=Integer.parseInt(strDrv[i]);
+                    }
+                    dvrIndex++;
+                }
+
+                index++;
+            }
+            // now initialize everything to the correct variables
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found - invalid configuration file");
+        } catch (IOException e) {
+            System.out.println("Error reading file..");
+        }
+
+    }
+
+    public static void printClientConfig(){
+        // this prints the current server configuration
+        System.out.println("Client Node number : " + nodeNum);
+        System.out.println("Num DVR entries : " + numDvrEntries);
+        // for each entry
+        for(int i =0; i<dvrEntries.length; i++){
+            // now loop again
+            for(int j = 0; j<dvrEntries[i].length; j++){
+                System.out.print(dvrEntries[i][j] + " ");
+            }
+            System.out.println("");
+        }
 
     }
 }
